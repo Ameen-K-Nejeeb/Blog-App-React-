8870cfbe236c8367abf89e190d23ac7071886647
DUDEamee <ameenameen8701@gmail.com>
2026-04-11 20:04:11 +0530

    Add : Home(profile) modal is added

--- src/utils/Modal.jsx
+++ src/utils/Modal.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 
-const Modal = ({children, modal, setModal, hidden}) => {
+const Modal = ({ children, modal, setModal }) => {
   return (
     <>
-    <div 
-      onClick={() => setModal(false)}
-    className={`bg-white/50 fixed insert-0 z-10 
-     transition-all duration-500 ${hidden}`}/>
-      <div className="relative z-50">{children}</div>
+      {/* Background Overlay - Changed to Gray with 50% opacity */}
+      <div
+        onClick={() => setModal(false)}
+        className={`fixed inset-0 z-10 bg-gray-200/50 transition-all duration-500 ${
+          modal ? "opacity-100 visible" : "opacity-0 invisible"
+        }`}
+      />
+
+      {/* Modal Content */}
+      <div
+        className={`fixed inset-0 z-50 flex items-center justify-center pointer-events-none transition-all duration-500 ${
+          modal ? "opacity-100 scale-100" : "opacity-0 scale-95"
+        }`}
+      >
+        {/* 'pointer-events-auto' allows interaction with the actual modal content */}
+        <div className="pointer-events-auto">
+          {children}
+        </div>
+      </div>
     </>
   )
 }
